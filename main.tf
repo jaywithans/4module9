65362424be2f38b44d08dfd8114f41d5f3a01f26
yangkevin005 <yangkevin005@gmail.com>
2025-08-02 20:23:13 -0400

    Azure nsg

--- main.tf
+++ main.tf
@@ -71,3 +71,77 @@ resource "azurerm_subnet" "US-West-Public" {
   virtual_network_name = azurerm_virtual_network.Group4-US-West.name
   address_prefixes     = ["10.0.3.0/24"]
 }
+
+################################
+# Azure NSG
+################################
+
+#East NSG
+resource "azurerm_network_security_group" "East-NSG" {
+  name                = "East-NSG"
+  location            = var.azure_region_1
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  security_rule {
+    name                       = "Allow-rdp"
+    priority                   = 1100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["3389"]
+    source_address_prefixes    = ["*"]
+    destination_address_prefix = "*"
+  }
+    security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["22"]
+    source_address_prefixes    = ["*"]
+    destination_address_prefix = "*"
+  }
+}
+
+#NSG association to Public Subnet
+resource "azurerm_subnet_network_security_group_association" "East-NSG-ASSOC" {
+  subnet_id                 = azurerm_subnet.US-East-Public.id
+  network_security_group_id = azurerm_network_security_group.East-NSG.id
+}
+
+#West NSG
+resource "azurerm_network_security_group" "West-NSG" {
+  name                = "West-NSG"
+  location            = var.azure_region_2
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  security_rule {
+    name                       = "Allow-rdp"
+    priority                   = 1100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["3389"]
+    source_address_prefixes    = ["*"]
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["22"]
+    source_address_prefixes    = ["*"]
+    destination_address_prefix = "*"
+  }
+}
+
+#NSG association to Public Subnet
+resource "azurerm_subnet_network_security_group_association" "West-NSG-ASSOC" {
+  subnet_id                 = azurerm_subnet.US-West-Public.id
+  network_security_group_id = azurerm_network_security_group.West-NSG.id
+}
