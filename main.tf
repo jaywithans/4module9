dcdc5a2c9f123e34252aad826bd5025c66aa5c3c
yangkevin005 <yangkevin005@gmail.com>
2025-08-03 21:53:23 -0400

    Added VM

--- main.tf
+++ main.tf
@@ -145,3 +145,179 @@ resource "azurerm_subnet_network_security_group_association" "West-NSG-ASSOC" {
   subnet_id                 = azurerm_subnet.US-West-Public.id
   network_security_group_id = azurerm_network_security_group.West-NSG.id
 }
+
+################################
+# Azure Network Interfaces
+################################
+
+resource "azurerm_network_interface" "East-Windows-NIC" {
+  name                = "East-Windows-NIC"
+  location            = var.azure_region_1
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.US-East-Public.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "East-Linux-NIC" {
+  name                = "East-Linux-NIC"
+  location            = var.azure_region_1
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.US-East-Public.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "West-Windows-NIC" {
+  name                = "West-Windows-NIC"
+  location            = var.azure_region_2
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.US-West-Public.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "West-Linux-NIC" {
+  name                = "West-Linux-NIC"
+  location            = var.azure_region_2
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.US-West-Public.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+################################
+# Azure Public IPs
+################################
+
+resource "azurerm_public_ip" "East-Windows-PIP" {
+  name                = "East-Windows-PIP"
+  location            = var.azure_region_1
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_public_ip" "East-Linux-PIP" {
+  name                = "East-Linux-PIP"
+  location            = var.azure_region_1
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_public_ip" "West-Windows-PIP" {
+  name                = "West-Windows-PIP"
+  location            = var.azure_region_2
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_public_ip" "West-Linux-PIP" {
+  name                = "West-Linux-PIP"
+  location            = var.azure_region_2
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  allocation_method   = "Static"
+}
+
+################################
+# Azure Virtual Machines
+################################
+
+resource "azurerm_windows_virtual_machine" "East-Windows-VM" {
+  name                = "East-Windows-VM"
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  location            = var.azure_region_1
+  size                = "Standard_B1s"
+  admin_username      = var.azure_windows_username
+  admin_password      = var.azure_windows_password
+  network_interface_ids = [
+    azurerm_network_interface.East-Windows-NIC.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-datacenter-gensecond"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "East-Linux-VM" {
+  name                = "East-Linux-VM"
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  location            = var.azure_region_1
+  size                = "Standard_B1s"
+  admin_username      = var.azure_linux_username
+  admin_password      = var.azure_linux_password
+  network_interface_ids = [
+    azurerm_network_interface.East-Linux-NIC.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "24.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "West-Windows-VM" {
+  name                = "West-Windows-VM"
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  location            = var.azure_region_2
+  size                = "Standard_B1s"
+  admin_username      = var.azure_windows_username
+  admin_password      = var.azure_windows_password
+  network_interface_ids = [
+    azurerm_network_interface.West-Windows-NIC.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-datacenter-gensecond"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "West-Linux-VM" {
+  name                = "West-Linux-VM"
+  resource_group_name = azurerm_resource_group.Group4-tf.name
+  location            = var.azure_region_2
+  size                = "Standard_B1s"
+  admin_username      = var.azure_linux_username
+  admin_password      = var.azure_linux_password
+  network_interface_ids = [
+    azurerm_network_interface.West-Linux-NIC.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "24.04-LTS"
+    version   = "latest"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -9,25 +9,47 @@ variable "aws_region_1" {
   description = "The AWS region where resources will be created."
   type        = string
   default     = ""
-  
+
 }
 
 variable "aws_region_2" {
   description = "The AWS region where resources will be created."
-  type        = string
+  type        = string  
   default     = ""
-  
 }
 
 variable "azure_region_1" {
   description = "The Azure region where resources will be created."
   type        = string
   default     = "East US"
-  
 }
 
 variable "azure_region_2" {
   description = "The Azure region where resources will be created."
   type        = string
   default     = "West US"
+}
+
+variable "azure_windows_username" {
+  description = "The username for the Azure Windows VM."
+  type        = string
+  default     = "azureuser"
+}
+
+variable "azure_windows_password" {
+  description = "The password for the Azure Windows VM."
+  type        = string
+  default     = "Hipposinspace27!"
+}
+
+variable "azure_linux_username" {
+  description = "The username for the Azure Linux VM."
+  type        = string
+  default     = "azureuser"
+}
+
+variable "azure_linux_password" {
+  description = "The password for the Azure Linux VM."
+  type        = string
+  default     = "Hipposinspace27!"
 }
